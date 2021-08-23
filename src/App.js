e7b9947adfb6f1d5096cafc62067251a1e4080f1
alstonwhite <59372363+alstonwhite@users.noreply.github.com>
2021-08-22 22:32:11 -0400

    Updated matches display format -- intermediary

--- src/App.js
+++ src/App.js
@@ -8,17 +8,6 @@ import { ladder } from './testData'
 
 
 function App() {
-
-  // const [ladder, setLadder] = useState(null)
-
-  console.log("ladder isArray", Array.isArray(ladder))
-  console.log("ladder array", ladder)
-
-  console.log("players isArray", Array.isArray(ladder.players))
-  console.log("players array", ladder.players)
-
-  console.log("matches isArray", Array.isArray(ladder.matches))
-  console.log("matches array", ladder.matches)
   
   return (
     <>
--- src/components/MatchesDisplay.js
+++ src/components/MatchesDisplay.js
@@ -8,15 +8,31 @@ const LadderRankings = ({matches}) => {
 
     
     return (
-        <>
-      <h3>Games</h3>
-      <ul>
+      <div className="matches-grid">
+        <h3 className="matches-grid__title">Games</h3>
+        <div className="matches-grid__heading">
+          <h4>Players</h4>
+          <h4>Date</h4>
+          <h4>Ranking</h4>
+          <h4>Score</h4>
+        </div>
         {matches ? matches.sort((p1, p2) => p1.ranking > p2.ranking ? 1 : -1).map(match => 
-          <li>Winner: {match.winner}, Loser: {match.loser}</li>) :
-          <li>No games added</li>
-        }  
-      </ul>
-        </>
+          <div className="matches-grid__game">  
+            <div className="matches-grid__player">
+              <div>{match.winner ? match.winner : "No winner entered"}</div>
+              <div>{match.date ? match.date : "No date entered"}</div>
+              <div>{match.winner.rank ? match.winner.rank : "No ranking entered"}</div>
+              <div>{match.winner.score ? match.winner.score : "No score entered"}</div>
+            </div>
+            <div className="matches-grid__player">
+              <div>{match.loser ? match.loser : "No winner entered"}</div>
+              <div></div>
+              <div>{match.loser.rank ? match.loser.rank : "No ranking entered"}</div>
+              <div>{match.loser.score ? match.loser.score : "No score entered"}</div>
+            </div>
+          </div>
+        ) : <div className="matches-grid__game">No matches added</div>} 
+      </div>
     )
 }
 
