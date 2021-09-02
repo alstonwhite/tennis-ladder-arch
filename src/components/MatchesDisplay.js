4ea6d26a06489d7cb47399bc7a70cc8fb772f009
alstonwhite <59372363+alstonwhite@users.noreply.github.com>
2021-09-01 23:14:13 -0400

    Added basic grid styling

--- src/components/MatchesDisplay.js
+++ src/components/MatchesDisplay.js
@@ -6,36 +6,38 @@ import { fetchMatches } from '../testData'
 const MatchesDisplay = ({ matches, players }) => {
     
     return (
-      <div className="matches-grid">
+      <div className="matches-grid-wrapper">
         <h3 className="matches-grid__title">Games</h3>
         <div className="matches-grid__heading">
-          <h4>Players</h4>
-          <h4>Date</h4>
-          <h4>Ranking</h4>
-          <h4>Score</h4>
+          <h4 className="player-head">Players</h4>
+          <h4 className="rank-head">Ranking</h4>
+          <h4 className="date-head">Date</h4>
+          <h4 className="score-head">Score</h4>
         </div>
-        {matches ? matches.sort((p1, p2) => p1.ranking > p2.ranking ? 1 : -1).map(match => 
-          <div className="matches-grid__game">  
-            <div className="matches-grid__player">
-              <div>{match.winner}</div>
-              <div>{match.date}</div>
-              <div>{players.find(player => player.name === match.winner).ranking}</div>
-              <div lassName="score-container">{match.winnerScore.map(score => 
-                <div className="score-item">{score}</div>
-              )}
+        <div className="matches-grid">
+          {matches ? matches.sort((p1, p2) => p1.ranking > p2.ranking ? 1 : -1).map(match => 
+            <div className="matches-grid__game">  
+              <div className="matches-grid__player">
+                <div className="player">{match.winner}</div>
+                <div className="rank">{players.find(player => player.name === match.winner).ranking}</div>
+                <div className="date">{match.date}</div>
+                <div className="score-container">{match.winnerScore.map(score => 
+                  <div className="score-item">{score}</div>
+                )}
+                </div>
+              </div>
+              <div className="matches-grid__player">
+                <div className="player">{match.loser ? match.loser : "No winner entered"}</div>
+                <div className="rank">{players.find(player => player.name === match.loser).ranking}</div>
+                <div className="date"></div>
+                <div className="score-container">{match.loserScore.map(score => 
+                  <div className="score-item">{score}</div>
+                )}
+                </div>
               </div>
             </div>
-            <div className="matches-grid__player">
-              <div>{match.loser ? match.loser : "No winner entered"}</div>
-              <div></div>
-              <div>{players.find(player => player.name === match.loser).ranking}</div>
-              <div className="score-container">{match.loserScore.map(score => 
-                <div className="score-item">{score}</div>
-              )}
-              </div>
-            </div>
-          </div>
-        ) : <div className="matches-grid__game">No matches added</div>} 
+          ) : <div className="matches-grid__game">No matches added</div>} 
+        </div>
       </div>
     )
 }
